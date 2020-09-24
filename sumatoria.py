42b7422432834e5987d4324b38d39fe7a14e1045
Jimmy Salas <ate.salas20@gmail.com>
2020-09-23 22:29:10 -0600

    TC3

--- sumatoria.py
+++ sumatoria.py
@@ -1,6 +1,16 @@
 def sumatoria_cubica(n):
+    r = 0
+    for i in range(1, n + 1):
+        for j in range(1, i + 1):
+            for k in range(j, i + j + 1):
+                r += 1     
+    return(r)
     raise NotImplementedError()
+    
 
 def sumatoria_constante(n):
+    i = n*(n+1)
+    j = (n*(n+1)/3)
+    res = int(i*j)
+    return res
     raise NotImplementedError()
-
